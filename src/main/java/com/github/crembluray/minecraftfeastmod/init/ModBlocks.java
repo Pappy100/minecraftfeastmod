60f9fe4bfe5c9460fee4224e883520c12eaab7e2
CremBluRay <jhoggard2003@gmail.com>
2019-07-27 19:44:04 -0500

    add yeast, dough, and flour items. remove redundant import statements

--- src/main/java/com/github/crembluray/minecraftfeastmod/init/ModBlocks.java
+++ src/main/java/com/github/crembluray/minecraftfeastmod/init/ModBlocks.java
@@ -1,10 +1,7 @@
 package com.github.crembluray.minecraftfeastmod.init;
 
-import com.github.crembluray.minecraftfeastmod.blocks.BlockBase;
 import com.github.crembluray.minecraftfeastmod.blocks.BlockCustomCake;
 import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-import net.minecraft.creativetab.CreativeTabs;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/com/github/crembluray/minecraftfeastmod/init/ModItems.java
+++ src/main/java/com/github/crembluray/minecraftfeastmod/init/ModItems.java
@@ -1,6 +1,8 @@
 package com.github.crembluray.minecraftfeastmod.init;
 
+import com.github.crembluray.minecraftfeastmod.items.ItemBase;
 import com.github.crembluray.minecraftfeastmod.items.ItemCustomFood;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
 import java.util.ArrayList;
@@ -11,5 +13,8 @@ public class ModItems {
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
     public static final Item BREAD_ROLL = new ItemCustomFood("bread_roll", 3, 0.3f, false);
+    public static final Item YEAST = new ItemBase("yeast", CreativeTabs.FOOD);
+    public static final Item DOUGH = new ItemBase("dough", CreativeTabs.FOOD);
+    public static final Item FLOUR = new ItemBase("flour", CreativeTabs.FOOD);
 
 }
