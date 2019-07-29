e6c8a37654335f18b7a93bf0fdd3b7d96faada5d
CremBluRay <jhoggard2003@gmail.com>
2019-07-29 01:22:01 -0500

    entity implementation, turkey mob, and burger item

--- src/main/java/com/github/crembluray/minecraftfeastmod/init/ModItems.java
+++ src/main/java/com/github/crembluray/minecraftfeastmod/init/ModItems.java
@@ -2,6 +2,7 @@ package com.github.crembluray.minecraftfeastmod.init;
 
 import com.github.crembluray.minecraftfeastmod.items.ItemBase;
 import com.github.crembluray.minecraftfeastmod.items.ItemCustomFood;
+import com.github.crembluray.minecraftfeastmod.items.ItemEffectFood;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 
@@ -12,9 +13,13 @@ public class ModItems {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
 
+    // Food
     public static final Item BREAD_ROLL = new ItemCustomFood("bread_roll", 3, 0.3f, false);
+    public static final ItemEffectFood DOUGH = new ItemEffectFood("dough", 1, 15, 0.3f, false);
+    public static final ItemCustomFood BURGER = new ItemCustomFood("burger", 8, 0.8f, false);
+
+    // Non-edible foodstuffs
     public static final Item YEAST = new ItemBase("yeast", CreativeTabs.FOOD);
-    public static final Item DOUGH = new ItemBase("dough", CreativeTabs.FOOD);
     public static final Item FLOUR = new ItemBase("flour", CreativeTabs.FOOD);
 
 }
