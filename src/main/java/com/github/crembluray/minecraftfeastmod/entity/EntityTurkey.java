a8017f0fcf06d3bce68fe772bb04fb3f3c616885
CremBluRay <jhoggard2003@gmail.com>
2019-07-29 02:49:06 -0500

    turkey animations

--- src/main/java/com/github/crembluray/minecraftfeastmod/entity/EntityTurkey.java
+++ src/main/java/com/github/crembluray/minecraftfeastmod/entity/EntityTurkey.java
@@ -1,15 +1,49 @@
 package com.github.crembluray.minecraftfeastmod.entity;
 
+import com.google.common.collect.Sets;
 import net.minecraft.entity.EntityAgeable;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
+import java.util.Set;
+
 public class EntityTurkey extends EntityChicken {
 
+    private static final Set<Item> TEMPTATION_ITEMS = Sets.newHashSet(Items.WHEAT_SEEDS, Items.MELON_SEEDS, Items.PUMPKIN_SEEDS, Items.BEETROOT_SEEDS);
     public EntityTurkey(World worldIn) {
         super(worldIn);
+        this.setSize(0.5F, 0.7F);
+    }
+
+    @Override
+    protected void initEntityAI() {
+        this.tasks.addTask(0, new EntityAISwimming(this));
+        this.tasks.addTask(1, new EntityAIPanic(this, 1.4D));
+        this.tasks.addTask(2, new EntityAIMate(this, 1.0D));
+        this.tasks.addTask(3, new EntityAITempt(this, 1.0D, false, TEMPTATION_ITEMS));
+        this.tasks.addTask(4, new EntityAIFollowParent(this, 1.1D));
+        this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 1.0D));
+        this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
+        this.tasks.addTask(7, new EntityAILookIdle(this));
+    }
+
+    @Override
+    protected void applyEntityAttributes() {
+        super.applyEntityAttributes();
+        this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10D);
+        this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.28D);
+    }
+
+    @Override
+    public float getEyeHeight() {
+        return this.height;
     }
 
     @Override
