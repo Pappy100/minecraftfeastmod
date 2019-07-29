5b94779ecadacb6e46de86f86d022601fa65f897
CremBluRay <jhoggard2003@gmail.com>
2019-07-29 14:17:26 -0500

    turkey drops, turkey sounds

--- src/main/java/com/github/crembluray/minecraftfeastmod/entity/EntityTurkey.java
+++ src/main/java/com/github/crembluray/minecraftfeastmod/entity/EntityTurkey.java
@@ -9,14 +9,25 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
 import java.util.Set;
 
 public class EntityTurkey extends EntityChicken {
 
+    private final ResourceLocation GOBBLE = new ResourceLocation("feastmod", "gobbling");
+    private final SoundEvent TURKEY_AMBIENT = new SoundEvent(GOBBLE);
+    private final ResourceLocation HURT = new ResourceLocation("feastmod", "turkey_hurt");
+    private final SoundEvent TURKEY_HURT = new SoundEvent(HURT);
+
+    private final ResourceLocation TURKEY_LOOT = new ResourceLocation("feastmod", "turkey_loot");
+
     private static final Set<Item> TEMPTATION_ITEMS = Sets.newHashSet(Items.WHEAT_SEEDS, Items.MELON_SEEDS, Items.PUMPKIN_SEEDS, Items.BEETROOT_SEEDS);
+
     public EntityTurkey(World worldIn) {
         super(worldIn);
         this.setSize(0.5F, 0.7F);
@@ -53,11 +64,45 @@ public class EntityTurkey extends EntityChicken {
 
     @Override
     protected SoundEvent getAmbientSound() {
-        return super.getAmbientSound();
+        return TURKEY_AMBIENT;
     }
 
     @Override
     protected SoundEvent getHurtSound(DamageSource source) {
-        return super.getHurtSound(source);
+        return TURKEY_HURT;
+    }
+
+    @Override
+    protected SoundEvent getDeathSound() {
+        return TURKEY_AMBIENT;
+    }
+
+    @Override
+    public void onLivingUpdate() {
+        super.onLivingUpdate();
+        this.oFlap = this.wingRotation;
+        this.oFlapSpeed = this.destPos;
+        this.destPos = (float)((double)this.destPos + (double)(this.onGround ? -1 : 4) * 0.3D);
+        this.destPos = MathHelper.clamp(this.destPos, 0.0F, 1.0F);
+
+        if (!this.onGround && this.wingRotDelta < 1.0F)
+        {
+            this.wingRotDelta = 1.0F;
+        }
+
+        this.wingRotDelta = (float)((double)this.wingRotDelta * 0.9D);
+
+        if (!this.onGround && this.motionY < 0.0D)
+        {
+            this.motionY *= 0.6D;
+        }
+
+        this.wingRotation += this.wingRotDelta * 2.0F;
+    }
+
+    @Nullable
+    @Override
+    protected ResourceLocation getLootTable() {
+        return TURKEY_LOOT;
     }
 }
